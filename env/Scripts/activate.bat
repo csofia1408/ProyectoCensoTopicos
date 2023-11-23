667e4ea1e1e0dacbb43712ee2102b4085892da7a
vmbotero <83251177+vmbotero@users.noreply.github.com>
2023-11-23 16:00:26 -0500

    Dev

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\csofi\Documents\ProyectoTopicosCenso\ECS_project\env
+set VIRTUAL_ENV=C:\Users\Victo\Downloads\ECS\ProyectoCensoTopicos\env
 
 if not defined PROMPT set PROMPT=$P$G
 
